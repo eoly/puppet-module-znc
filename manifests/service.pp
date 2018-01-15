e1597daa741ffeb15c70d53775ddb7922ffcb1df
Eric Olsen <ericjolsen@gmail.com>
2018-01-14 21:33:12 -0500

    wip

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,6 +9,15 @@ class znc::service {
 
   if $service_manage {
 
+    # Manually set service provider to systemd on Amazon Linux 2
+    # which is based off el7 and includes systemd.
+    # See issue PUP-8248 - https://tickets.puppetlabs.com/browse/PUP-8248
+    if ($::facts['os']['name'] == 'Amazon') and ($::facts['os']['release']['major'] == '2') {
+      $service_provider = 'systemd'
+    } else {
+      $service_provider = undef
+    }
+
     if $service_ensure == 'running' {
       $service_enable = true
     } else {
@@ -17,10 +26,11 @@ class znc::service {
 
     service { 'znc':
       ensure     => $service_ensure,
+      name       => $service_name,
       enable     => $service_enable,
+      provider   => $service_provider,
       hasstatus  => true,
       hasrestart => true,
-      name       => $service_name,
     }
   }
 }
--- spec/classes/znc_spec.rb
+++ spec/classes/znc_spec.rb
@@ -1,6 +1,36 @@
 require 'spec_helper'
 
 describe 'znc' do
+  platforms = {
+    'amazon_linux2' => {
+      :facts_hash => {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'Amazon',
+        :operatingsystemmajrelease => '2',
+        :os => {
+          'family' => 'RedHat',
+          'name'   => 'Amazon',
+          'release' => {
+            'major' => '2',
+          },
+        },
+      },
+    },
+    'el7' => {
+      :facts_hash => {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :operatingsystemmajrelease => '7',
+        :os => {
+          'family' => 'RedHat',
+          'release' => {
+            'major' => '7',
+          },
+        },
+      },
+    }
+  }
+
   context 'on unsupported os family' do
     let(:facts) do
       {
@@ -13,11 +43,13 @@ describe 'znc' do
     end
   end
 
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
+  describe 'with defaults' do
+    platforms.sort.each do |k,v|
+      context "#{k}" do
+        let(:facts) do
+          v[:facts_hash]
+        end
 
-      describe 'with defaults' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('znc') }
         it { is_expected.to contain_class('znc::params') }
@@ -26,10 +58,18 @@ describe 'znc' do
 
         it { is_expected.to contain_package('znc').with_ensure('installed') }
 
+        if (v[:facts_hash][:os]['name'] == 'Amazon') and (v[:facts_hash][:os]['release']['major'] == '2')
+          it 'manually sets provider to systemd' do
+            is_expected.to contain_service('znc').with(
+              'provider' => 'systemd',
+            )
+          end
+        end
+
         it {
           is_expected.to contain_service('znc').with(
-            'ensure' => 'running',
-            'enable' => 'true',
+            'ensure'   => 'running',
+            'enable'   => 'true',
           )
         }
       end
